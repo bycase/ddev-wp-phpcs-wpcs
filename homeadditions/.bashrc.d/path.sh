95a27adf975354f72b6b783639aa3d89b39a09fe
bycase <dev@bycase.ca>
2025-01-15 20:59:13 +0000

    Added commands for php, phpcs and phpcbf

---
+++ homeadditions/.bashrc.d/path.sh
@@ -0,0 +1,7 @@
+# #ddev-generated
+# Adding the formatting commands into your path
+
+export PATH=$PATH:/usr/local/composer/vendor/bin/phpcs
+export PATH=$PATH:/usr/local/composer/vendor/bin/phpcbf
+export PATH=$PATH:/usr/local/composer/vendor/squizlabs/php_codesniffer
+export PATH=$PATH:/usr/local/composer/vendor/squizlabs/php_codesniffer/bin
